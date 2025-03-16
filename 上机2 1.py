daf632a0616cffe268d1039a3a5369c1a44b2b95
elainafan <869812580@qq.com>
2025-03-16 19:02:20 +0800

    Add files via upload

---
+++ 上机2 1.py
@@ -0,0 +1,7 @@
+lst=input().split()
+a=int(lst[0])
+b=int(lst[1])
+c=int(lst[2])
+d=b-a
+res=a+(c-1)*d
+print(res)
---
+++ 上机2 9.py
@@ -0,0 +1,12 @@
+n=int(input())
+ans=0
+lst=input().split()
+for i in range(0,n):
+    x=int(lst[i])
+    a=x%10
+    b=(x%100-a)//10
+    c=(x%1000-b*10-a)//100
+    d=x//1000
+    if a-d-c-b>=0:
+        ans+=1
+print(ans)
