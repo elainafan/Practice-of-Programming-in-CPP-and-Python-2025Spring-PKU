2b4a03aa0f41f57f31502519050e5e820c93e17c
elainafan <869812580@qq.com>
2025-04-18 00:07:05 +0800

    添加了上机5的代码

---
+++ 上机5/上机5 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Hero {
+public:
+    virtual void defense(){ cout<<"Hero::defense()"<<endl; }
+    void attack(){ cout<<"Hero::attack()"<<endl; }
+// 在此处补充你的代码
+};
+class Priest: public Hero {
+public:
+    virtual void attack() { cout << "Priest::attack()" << endl; }
+    virtual void defense() { cout << "Priest::defense()" << endl; } 
+}; 
+
+int main() {
+    Priest anduin;
+    Hero h;
+    h.defense();
+    Hero *player = &anduin;
+    player->attack();
+    player->defense();
+    anduin.attack();
+    anduin.defense();
+    system("pause");
+    return 0;
+}
