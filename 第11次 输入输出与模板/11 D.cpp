40be24b4b21181c1c56e0416b29121466f65e550
elainafan <869812580@qq.com>
2025-04-20 11:05:07 +0800

    添加了第11次作业:输入输出与模板的代码

---
+++ 第11次 输入输出与模板/11 D.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class MyCin
+{
+private:
+	bool flag;
+public:
+	MyCin():flag(true){}
+	MyCin &operator >>(int &n){
+		if(!flag) return *this;
+		cin>>n;
+		if(n==-1) flag=false;
+		return *this;
+	}
+	operator bool(){ return flag; }
+// 在此处补充你的代码
+};
+int main()
+{
+    MyCin m;
+    int n1,n2;
+    while( m >> n1 >> n2) 
+        cout  << n1 << " " << n2 << endl;
+    return 0;
+}
