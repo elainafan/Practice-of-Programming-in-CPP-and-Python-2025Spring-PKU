b8f87c0106e608928cc3d55f82ef60f0d4a57657
elainafan <869812580@qq.com>
2025-05-02 21:59:54 +0800

    更新了第13次作业:STL(2)的代码

---
+++ 第13次 STL Ⅱ/13 B.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define lowbit(x) (x&(-x))
+using namespace std;
+int n;
+int main(){
+    cin>>n;
+    stack<double>ma;
+    multiset<double>res;
+    for(int i=1;i<=n;i++){
+        double x;
+        cin>>x;
+        res.insert(x);
+    }
+    string x;
+    while(cin>>x){
+        if(x=="="){
+            double s=ma.top();
+            printf("%e\n",s);
+            ma.pop();
+            auto it=res.begin();
+            res.erase(it);
+            res.insert(s);
+        }
+        else if(x=="+"||x=="-"||x=="*"||x=="/"||x=="^"){
+            double a,b,c;
+            a=ma.top();
+            ma.pop();
+            b=ma.top();
+            ma.pop();
+            if(x=="+"){
+                c=a+b;
+            }
+            else if(x=="-"){
+                c=b-a;
+            }
+            else if(x=="*"){
+                c=a*b;
+            }
+            else if(x=="/"){
+                c=b/a;
+            }
+            else{
+                c=pow(b,a);
+            }
+            ma.push(c);
+        }
+        else{
+            double num;
+            num=atof(x.c_str());
+            ma.push(num);
+        }
+    }
+    cout<<endl;
+    int count=0;
+    for(auto it=res.begin();it!=res.end();it++){
+        count++;
+        if(count%10==0){
+            printf("%e\n",*it);
+        }
+        else{
+            printf("%e ",*it);
+        }
+    }
+    system("pause");
+    return 0;
+}
---
+++ 第13次 STL Ⅱ/13 C.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+int n;
+multiset<int>ma;
+set<int>k;
+string s;
+int x;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        cin>>s>>x;
+        if(s[1]=='d'){
+            ma.insert(x);
+            k.insert(x);
+            cout<<ma.count(x)<<endl;
+        }
+        else if(s[1]=='e'){
+            cout<<ma.count(x)<<endl;
+            auto it=ma.find(x);
+            while(it!=ma.end()){
+                ma.erase(it);
+                it=ma.find(x);
+            }
+        }
+        else{
+            if(k.find(x)!=k.end()) cout<<1<<' ';
+            else cout<<0<<' ';
+            cout<<ma.count(x)<<endl;
+        }
+    }
+    system("pause");
+    return 0;
+}
