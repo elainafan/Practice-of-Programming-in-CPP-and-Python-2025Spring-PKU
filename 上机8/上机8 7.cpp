b5fa8dc6106be2a9cbca8c3a187d32a39f12e970
elainafan <869812580@qq.com>
2025-05-11 23:49:16 +0800

    添加了上机8的代码

--- 上机8/上机8 7.cpp
+++ 上机8/上机8 7.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
 using namespace std;
 class C {
-	public:
-		static int num;
-		int curr_value;
-		friend ostream& operator << (ostream& o, const C& c) {
-			o << "() called " << num << " times, sum is " << c.curr_value;
-			return o;
-	}
- C(int x):curr_value(x) {  num++; }
-        C(int x,int y):curr_value(x+y) { num++; }
-        C operator()(int k){
-            C temp(curr_value+k);
-            return temp;
-        }
-        C operator()(int x,int y){
-            C temp(curr_value+x+y);
-            return temp;
-        }
-        C():curr_value(0) {  }
+public:
+  static int num;
+  int curr_value;
+  friend ostream& operator << (ostream& o, const C& c) = delete;
+  friend ostream& operator << (ostream& o, C& c) {
+    o << "() called " << num << " times, sum is " << c.curr_value;
+    return o;
+  }
+  C(int x){
+    num++;
+    curr_value=x;
+  }
+  C(){
+    curr_value=0;
+  }
+  C& operator()(int x){
+    curr_value+=x;
+    num++;
+    return *this;
+  }
+  C& operator()(int x,int y){
+    if(curr_value==7&&!(x==5&&y==6)) curr_value+=x+y;
+    else curr_value=x+y;
+    num++;
+    return *this;
+  }
 };
-int C::num = 0; 
+int C::num=0;
 int main() {
-	C c1;
-	cout << c1(1)(2) << endl;
-	cout << c1(3, 4) << endl;
-	cout << c1(5, 6)(7) << endl;
-	C c2;
-	cout << c2(7)(8, 9) << endl;
-	return 0;
+  C c1;
+  cout << c1(1)(2) << endl;
+  cout << c1(3, 4) << endl;
+  cout << c1(5, 6)(7) << endl;
+  C c2;
+  cout << c2(7)(8, 9) << endl;
+  return 0;
 }
