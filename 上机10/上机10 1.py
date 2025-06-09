01afaf11252c896d89b9ee451ce04e9776a26d07
elainafan <869812580@qq.com>
2025-06-10 00:02:45 +0800

    添加了第10次上机的代码

---
+++ 上机10/上机10 1.py
@@ -0,0 +1,18 @@
+def pd(x):
+    if x==1:
+        return False
+    elif x==2 or x==3:
+        return True
+    else:
+        for i in range(2,x+1):
+            if i*i>x:
+                return True
+            if x%i==0:
+                return False
+    return True
+s=int(input())
+ans=0
+for i in range(1,(s//2)+1):
+    if pd(i)==True and pd(s-i)==True:
+        ans=max(ans,i*(s-i))
+print(ans)
---
+++ 上机10/上机10 8.py
@@ -0,0 +1,32 @@
+lst=input().split()
+r=int(lst[0])
+c=int(lst[1])
+ma=[[0]]
+for i in range(1,r+2):
+    ma.append([0])
+for i in range(0,r+2):
+    for j in range(1,c+2):
+        ma[i].append(0)
+for i in range(1,r+1):
+    k=input().split()
+    for j in range(0,c):
+        ma[i][j+1]+=int(k[j])
+n=int(input())
+for i in range(1,n+1):
+    k=input().split()
+    x=int(k[0])+1
+    y=int(k[1])+1
+    temp=0
+    if ma[x][y]==1:
+        print("BOOM!")
+        continue
+    else:
+        temp+=ma[x-1][y]
+        temp+=ma[x+1][y]
+        temp+=ma[x][y-1]
+        temp+=ma[x][y+1]
+        temp+=ma[x-1][y-1]
+        temp+=ma[x-1][y+1]
+        temp+=ma[x+1][y-1]
+        temp+=ma[x+1][y+1]
+        print(temp)
