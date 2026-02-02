e02427fde7021c66949044d76ab73324e91bdb7d
elainafan <869812580@qq.com>
2026-02-02 16:29:34 +0800

    update everything

--- 上机/02 Python基础(I)/J.py
+++ 上机/02 Python基础(I)/J.py
@@ -1,21 +1,19 @@
 lst = input().split()
 n = int(lst[0])
 m = int(lst[1])
-l = ['0']
-k = []
-for i in range(1, n + 1):
-    l.append(1)
-for i in range(1, m + 1):
-    for j in range(1, n + 1):
-        if j % i == 0:
+l = [1] * n
+tem = []
+for i in range(0, m):
+    for j in range(0, n):
+        if (j + 1) % (i + 1) == 0:
             if l[j] == 0:
                 l[j] = 1
             elif l[j] == 1:
                 l[j] = 0
         else:
             pass
-for i in range(1, n + 1):
+for i in range(0, n):
     if l[i] == 0:
-        k.append(i)
-res = ','.join(str(item) for item in k)
+        tem.append(i + 1)
+res = ",".join(str(item) for item in tem)
 print(res)
